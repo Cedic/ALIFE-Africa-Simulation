e8742dc39a75730568ce4a89d588e3a073717e3a
Cedic <cedric.boscher@insa-lyon.fr>
2016-11-20 20:06:59 -0500

    Refactoring methodes

--- main.py
+++ main.py
@@ -6,15 +6,6 @@ from visual import *
 from constants import *
 import Image
 
-
-# Constants
-
-
-scene = display(title='Africa',
-     x=0, y=0, width=600, height=600,
-     center = (SIZE_AFRICA*VCOEFF/2, SIZE_AFRICA*VCOEFF/2,70), background=(0,0,0))
-
-
 # Func
 def generar_mat(size):
     mat = []
@@ -46,6 +37,15 @@ def inmat(i,j,mat):
         return True   
         
     
+def generar_un_recurso(mat, x, y, n, dict_recursos):
+	
+	mat[x][y] = calc_cantidad(n)
+	recurso_model = cylinder(pos=(x*VCOEFF,y*VCOEFF,0), axis=(0,0,n),
+							 radius=3, material = materials.wood)
+	dict_recursos[(x,y)] = recurso_model
+	
+	return mat
+	
 def generar_recursos(mat, puntos):
 
     dict_recursos = {}
@@ -53,63 +53,48 @@ def generar_recursos(mat, puntos):
     for pt in puntos:
         x = pt[0]
         y = pt[1]
-        mat[x][y] = calc_cantidad(50)
+        mat = generar_un_recurso(mat, x, y, 50, dict_recursos)
         for i in range(x-1,x+2):
             for j in range(y-1,y+2):
                 if [i,j] != [x,y]:
-                    mat[i][j] = calc_cantidad(45)
-                    recurso_model = cylinder(pos=(i*VCOEFF,j*VCOEFF,0), axis=(0,0,45), radius=3, material = materials.wood)
-                    dict_recursos[(i,j)] = recurso_model
+					mat = generar_un_recurso(mat, i, j, 45, dict_recursos)
         for i in range(x-2,x+3):
             for j in range(y-2,y+3):
                 if mat[i][j] == 0:
-                    mat[i][j] = calc_cantidad(35)
-                    recurso_model = cylinder(pos=(i*VCOEFF,j*VCOEFF,0), axis=(0,0,35), radius=3, material = materials.wood)
-                    dict_recursos[(i,j)] = recurso_model
+                    mat = generar_un_recurso(mat, i, j, 35, dict_recursos)
         for i in range(x-3,x+4):
             for j in range(y-3,y+4):
                 if inmat(i,j,mat):
                     if mat[i][j] == 0:
-                        mat[i][j] = calc_cantidad(20)
-                        recurso_model = cylinder(pos=(i*VCOEFF,j*VCOEFF,0), axis=(0,0,35), radius=3, material = materials.wood)
-                        dict_recursos[(i,j)] = recurso_model
+                        mat = generar_un_recurso(mat, i, j, 20, dict_recursos)
         for i in range(x-4,x+5):
             for j in range(y-4,y+5):
                 if inmat(i,j,mat):
                     if mat[i][j] == 0:
-                        mat[i][j] = calc_cantidad(5)
-                        recurso_model = cylinder(pos=(i*VCOEFF,j*VCOEFF,0), axis=(0,0,5), radius=3, material = materials.wood)
-                        dict_recursos[(i,j)] = recurso_model
+                        mat = generar_un_recurso(mat, i, j, 5, dict_recursos)
 
         
     return mat, dict_recursos
 
 
-def generar_suelo(tab):
+def generar_suelo():
 	suelo = box (pos=(SIZE_AFRICA*VCOEFF/2,SIZE_AFRICA*VCOEFF/2,-10), 
 				 length=SIZE_AFRICA*VCOEFF + 10, height=SIZE_AFRICA*VCOEFF +10,
 				 width=10, material = materials.marble, color = color.orange)
 
-	
-def printtab(tab):
-	# for i in range(len(tab)):
-	#     print("")
-	#     for j in range(len(tab[i])):
-    #         print(tab[i][j], end="")
-    #         if tab[i][j] < 10:
-    #             print(" ", end="")
-    # print("")
+    
+scene = display(title='Africa',
+     x=0, y=0, width=600, height=600,
+     center = (SIZE_AFRICA*VCOEFF/2, SIZE_AFRICA*VCOEFF/2,70), background=(0,0,0))
+         
+matrix_ressources = generar_mat(SIZE_AFRICA)
+matrix_ressources, models_recursos = generar_recursos(matrix_ressources,
+													  CENTROS_RECURSOS)
+													  
+matrix_pollution = generar_mat(SIZE_AFRICA)
 
-    # Display
-	im = plt.imshow(tab, extent=[-1,1,-1,1], interpolation='none');
-	plt.xticks([]); plt.yticks([]);
-	plt.show()
-    
-    
-matriz = generar_mat(SIZE_AFRICA)
-matriz, models_recursos = generar_recursos(matriz, CENTROS_RECURSOS)
 
 
 #~ printtab(matriz)
 print(models_recursos)
-generar_suelo(matrix)
+generar_suelo()
