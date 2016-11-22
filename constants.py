21528f44d66b59e769faeaa9dac646307b94cc03
Nicolas Nadisic <nicolas.nadisic@gmail.com>
2016-11-21 20:15:09 -0500

    Ajout bouffe au milieu

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 SIZE_AFRICA = 100 # Size of the Area
-RESSOURCES_CENTERS = [[2,38],[94,2]]
+RESSOURCES_CENTERS = [[2,38],[94,2],[50,50]]
 VCOEFF = 10 # Visualization scale for VPython
 SIM_SPEED = 10
