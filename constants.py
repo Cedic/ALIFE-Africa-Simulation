d1c50220515bbf5d3a74cdea38f1ccfc91a72119
Nicolas Nadisic <nicolas.nadisic@gmail.com>
2016-11-24 18:58:51 -0500

    bah alors

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 SIZE_AFRICA = 100 # Size of the Area
-RESOURCES_CENTERS = [[2,38],[94,2],[50,50]]
+RESOURCES_CENTERS = [[2,10],[10,2],[15,15],[15,20],[89,8],[85,5],[94,2],[90,8]]
 VCOEFF = 10 # Visualization scale for VPython
 SIM_SPEED = 15000
 TEXTURE_RESIZE = 128
